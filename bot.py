93eba8dc79cfb86da2f1fc2850ff66288de3b4a2
Sam <75843086+wotanut@users.noreply.github.com>
2022-02-03 12:27:52 +0000

    Checks for status changes
    Bot now checks for status changes, will only publish a message if changed status to offline, will auto publish if enabled and all commands now have a help description

--- bot.py
+++ bot.py
@@ -7,6 +7,14 @@ from asyncio import sleep
 import asyncio
 from discord_slash import SlashCommand, SlashContext
 import re
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+cluster = MongoClient(os.environ.get("mongo"))
+db = cluster["discord"]
+collection = db["status"]
 
 
 intents = discord.Intents.all()  
@@ -19,13 +27,6 @@ bot = commands.Bot(
 )
 
 bot.author_id = 705798778472366131  # Change to your discord id!!!
-
-slash = SlashCommand(bot,sync_commands=True)
-
-@slash.slash(name="test")
-async def test(ctx: SlashContext):
-    embed = Embed(title="Embed Test")
-    await ctx.send(embed=embed)
 
 class MyHelp(commands.HelpCommand):
   def get_command_signature(self, command):
@@ -58,7 +59,7 @@ bot.help_command = MyHelp()
 @bot.event 
 async def on_ready():  # When the bot is ready
     print("I'm in")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name = f"with your bots!"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="over your bots!"))
     print(bot.user)  # Prints the bot's username and identifier
 
 @bot.command(help="Checks the status of a bot")
@@ -83,18 +84,9 @@ async def status(ctx,user:discord.User):
 async def ping(ctx):
   await ctx.send(f':ping_pong: Pong!\n **Bot**: {round(bot.latency * 1000)} ms')  
 
-@bot.event
-async def on_message(message):  
-  if message.author.id == bot.user.id :
-        return
-  
-  if message.content.startswith(f"<@!{bot.user.id}>") and len(message.content) == len(f"<@!{bot.user.id}>"):
-    await message.channel.send(f"Hi, my name is status checker, my prefix is s! and you can do s!help for help!")
-  await bot.process_commands(message)
-
 @commands.has_permissions(manage_guild=True)
-@bot.command()
-async def config(ctx):
+@bot.command(help="Adds a bot to watch for status changes")
+async def add_bot(ctx):
   await ctx.message.delete()
 
   def check(msg):
@@ -104,6 +96,10 @@ async def config(ctx):
   embed = discord.Embed(title = f"Status Checker", color= int("0x36393f", 16))
   embed.add_field(name = f"Channel:",value = f"None yet! tag one to select", inline = True)
   bed = await ctx.send(embed=embed)
+
+  # get the channel
+
+
   try:
         msg = await bot.wait_for("message",check=check, timeout=30)
         await msg.delete()
@@ -114,14 +110,11 @@ async def config(ctx):
           try:
             channel = bot.get_channel(int(channel))
           except Exception as e:
-            await ctx.send(f"Failed to get channel, this is usually becuase I do not have access or the channel does not exist Error: || {e} ||")
-          await ctx.send(channel.type)
+            await ctx.send(f"Failed to get channel, this is usually becuase I do not have access or the channel does not exist. \n Error: || {e} ||")
           if type(channel) != discord.channel.TextChannel:
             await ctx.send("That doesn't look like a text channel")
             await bed.delete()
             return
-          #elif discord.channel.TextChannel.is_news():
-           # await ctx.send("Anouncment catagory")
         except:
           await ctx.send("That doesn't look like a channel to me")
           await bed.delete()
@@ -132,6 +125,11 @@ async def config(ctx):
         await bed.edit(embed=embed)
   except asyncio.TimeoutError:
       await ctx.send("Sorry, you didn't reply in time!")
+
+
+
+  # get the bot
+
   try:
         input = await bot.wait_for("message",check=check, timeout=30)
         await input.delete()
@@ -141,6 +139,10 @@ async def config(ctx):
           user = user.replace(">","")
           user = user.replace("!","")
           user = bot.get_user(int(user))
+          if not user.bot:
+            await ctx.send("For privacy reasons I can only track bots")
+            await bed.delete()
+            return
           #await ctx.send(f"User's ID is {user.id} \n User Name \n {user.name} \n Mention {user.mention}")
         except:
           await ctx.send("That doesn't look like a bot to me")
@@ -153,39 +155,115 @@ async def config(ctx):
         await bed.edit(embed=embed)
   except asyncio.TimeoutError:
       await ctx.send("Sorry, you didn't reply in time!")
+
+
+  # get the down message
+
+
   try:
         down_message = await bot.wait_for("message",check=check, timeout=30)
         await down_message.delete()
         embed = discord.Embed(title = f"Status Checker", color= int("0x36393f", 16))
         embed.add_field(name = f"Channel:",value = f"{channel.mention}", inline = True)
         embed.add_field(name = f"Bot:",value = f"{user.mention}", inline = True)
-        embed.add_field(name = f"Bot:",value = f"{down_message.content}", inline = True)
+        embed.add_field(name = f"Down Message:",value = f"{down_message.content}", inline = True)
+        if channel.is_news():
+          embed.add_field(name = f"Auto Publish",value = f"I have detected that the channel you selected ({channel.mention}) is an announcement catagory, would you like me to automatically publish the down message?", inline = True)
         await bed.edit(embed=embed)
   except asyncio.TimeoutError:
       await ctx.send("Sorry, you didn't reply in time!")
-  message = await ctx.send(f"Watching {user.mention} I will aleart you if their status changes")
-  #bot.loop.create_task(checks(ctx,user, message))
-  
 
-async def checks(ctx,user, message):
-  while True:
-    for i in ctx.guild.members:
-      if i.id == user.id:
-        if str(i.status) == 'online':
-            await message.edit(content=f"<:online:844536822972284948>  {user.mention} is online")
-        elif str(i.status) == 'idle':
-          await message.edit(content=f"<:idle:852891603264602172> {user.mention} is on idle")
-        elif str(i.status) == 'dnd':
-          await message.edit(content=f"<:dnd:852891721771515934> {user.mention} is on do not disturb")
+
+  # get the state of auto publish (depending on type of channel)
+
+  auto_publish = False
+  if channel.is_news():
+    try:
+        publish = await bot.wait_for("message",check=check, timeout=30)
+        await publish.delete()
+
+        yes = ["y", "yes", "afirm","afirmitave", "positive","true"]
+        no = ["n", "no", "negative","false"]
+
+
+        if publish.content.lower() in yes:
+          auto_publish = True
+        elif publish.content.lower() in no:
+          auto_publish = False
+          await bed.delete()
+          return
         else:
-          await message.edit(content=f"<:offline:844536738512896020> {user.mention} is offline")
-    await sleep(10)
+          await ctx.send(f"I couldn't tell what that meant, you must pick from one of the following options {yes}{no}")
 
-@bot.command()
+        embed = discord.Embed(title = f"Status Checker", color= int("0x36393f", 16))
+        embed.add_field(name = f"Channel:",value = f"{channel.mention}", inline = True)
+        embed.add_field(name = f"Bot:",value = f"{user.mention}", inline = True)
+        embed.add_field(name = f"Bot:",value = f"{down_message.content}", inline = True)
+        embed.add_field(name = f"Auto Publish",value = f"{publish.content}", inline = True)
+        await bed.edit(embed=embed)
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you didn't reply in time!")
+        await bed.delete()
+        return
+  message = await channel.send(f"<a:loading:844891826934251551> Loading Status Checker information")
+
+  try:
+    collection.insert_one({"_id": user.id, f"{ctx.guild.id}": [channel.id,message.id,down_message.content,auto_publish]})
+  except Exception as e:
+    collection.update_one({"_id": user.id}, {"$set" : {f"{ctx.guild.id}": [channel.id,message.id,down_message.content,auto_publish]}})
+
+  await ctx.send(f"Watching {user.mention} I will alert you if their status changes")
+
+@bot.command(help="Invie the bot")
 async def invite(ctx):
   user = bot.get_user(ctx.author.id)
   await user.send("https://dsc.gg/status-checker")
   await ctx.message.add_reaction("<:success:844896295054213171>")
+
+@bot.event
+async def on_member_update(before,after):
+  if not before.bot:
+    return
+
+  if before.status == after.status:
+    return
+
+    
+  user = bot.get_user(before.id)
+  try:
+    results = collection.find()
+    for result in results:
+
+      if str(result["_id"]) != str(user.id):
+        # pass do literally nothing
+        pass
+      else:
+        for query in result:
+          if str(query) == "_id":
+            pass
+          else:
+            server = result[query]
+            channel = bot.get_channel(server[0])
+            msg = await channel.fetch_message(server[1])
+            down_message = server[2]
+            auto_publish = server[3]      
+            if str(after.status) == 'online':
+                await msg.edit(content=f"<:online:844536822972284948>  {user.mention} is online")
+            elif str(after.status) == 'idle':
+              await msg.edit(content=f"<:idle:852891603264602172> {user.mention} is on idle")
+            elif str(after.status) == 'dnd':
+              await msg.edit(content=f"<:dnd:852891721771515934> {user.mention} is on do not disturb")
+            else:
+              await msg.edit(content=f"<:offline:844536738512896020> {user.mention} is offline")
+              
+              down_msg = await channel.send(down_message)
+              if auto_publish == True:
+                print("publishing down message")
+                await down_msg.publish()
+            
+  except Exception as e:
+    print(e)
+    return
 
 
 extensions = ['cogs.loops']

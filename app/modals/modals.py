273190de2c150bf7d608d5621b79b4a7eb8ad44a
Sam <sambpilot@gmail.com>
2022-11-30 14:53:07 +0000

    improved modal to allow for testing

--- app/modals/modals.py
+++ app/modals/modals.py
@@ -4,7 +4,7 @@ import re
  
 # local imports
 
-from utilities.database import Database
+# from ..utilities.database import Database
 
 
 async def generate_json(NotificationType, NotificationTarget, NotificationPayload):
@@ -14,37 +14,31 @@ async def generate_json(NotificationType, NotificationTarget, NotificationPayloa
         "NotificationPayload": NotificationPayload
     }
 
-async def is_valid(interaction: discord.Interaction,NotificationType, NotificationTarget, bot: discord.Client):
+async def is_valid(NotificationType, NotificationTarget, bot: discord.Client):
     notificationTypes = ["dm","guild","webhook","email"]
 
     if NotificationType.lower() not in notificationTypes:
-        await interaction.response.send_message("Your notification type is invalid. Please try again.")
-        return
+        return "Your notification type is invalid. Please chose from the following: " + ", ".join(notificationTypes)
     
         # TODO: Change this to utilities.data.notificationType
     
-    if NotificationType.lower() == "dm":
-        try:
-            user = await bot.fetch_user(int(NotificationTarget))
-        except:
-            await interaction.response.send_message("Your notification target is invalid. Please try again.")
-            return
+        # IMPORTANT: There is no need to check if the notification target for a DM is valid, as it is the user's ID.
+    
     elif NotificationType.lower() == "guild":
         try:
             guild = await bot.fetch_guild(int(NotificationTarget))
         except:
-            await interaction.response.send_message("Your notification target is invalid. Please try again.")
-            return
+            return "Your notification target is invalid. Please try again."
     elif NotificationType.lower() == "webhook":
         regex = re.search(r"(http|https)://[a-zA-Z0-9./]+", NotificationTarget)
         if regex == None:
-            await interaction.response.send_message("Your notification target is invalid. Please try again.")
-            return
+            return "Your notification target is invalid. Please try again."
     elif NotificationType.lower() == "email":
         regex = re.search(r"[a-z0-9]+@[a-z0-9]+\.[a-z]+", NotificationTarget)
         if regex == None:
-            await interaction.response.send_message("Your notification target is invalid. Please try again.")
-            return
+            return "Your notification target is invalid. Please try again."
+
+    return True
 
 class Setup(ui.Modal,title="Setup"):
         # ServiceType = ui.Select(placeholder="Select a service type",min_values=1,max_values=1, options=[
@@ -54,68 +48,62 @@ class Setup(ui.Modal,title="Setup"):
         # ])
         # TODO: As soon as discord supports select menus in modals, uncomment this
 
-        ServiceType = ui.TextInput(label="Service Type (website,bot,mc)",placeholder="website",required=True,row=0)
+        ServiceType = ui.TextInput(label="Service Type (website,bot,minecraft)",placeholder="website",required=True)
         ServiceURL = ui.TextInput(label="Service URL (bot_name)#(discriminator) or url",placeholder="https://sblue.tech/uptime",required=False)
 
         NotificationType = ui.TextInput(label="Notification Type",placeholder="dm",required=True)
-        NotificationTarget = ui.TextInput(label="Notification Target",placeholder="845943691386290198",required=True)
+        NotificationTarget = ui.TextInput(label="Notification Target",placeholder="845943691386290198",required=False)
         NotificationPaylod = ui.TextInput(label="Notification Payload",placeholder="Your application is down!",required=True)
     
         
 
         async def on_submit(self, interaction: discord.Interaction):
+            valid = await is_valid(interaction,self.NotificationType.value, self.NotificationTarget.value, interaction.client)
+            if valid != True:
+                await interaction.response.send_message(valid, ephemeral=True)
+                return
+
             if self.ServiceType.value.lower() == "website":
-                ui.Modal.clear_items(self)
-                print("Clearing items")
-                self.add_item(ui.TextInput(label="Service URL (websites and mc only)",placeholder="https://sblue.tech/uptime",required=False))
-                print("Added item")
-            
-
-
-            # if await is_valid(interaction,self.NotificationType.value, self.NotificationTarget.value, interaction.client) == False:
-            #     return
-
-            # if self.ServiceType.value.lower() == "website":
-            #     if self.ServiceURL.value == None:
-            #         await interaction.response.send_message("Your service type is website but you did not provide a service URL. Please try again.")
-            #         return
-            #     regex = re.search(r"(http|https)://[a-zA-Z0-9./]+", self.ServiceURL.value)
-            #     if regex == None:
-            #         await interaction.response.send_message("Your service URL is invalid. Please try again.")
-            #         return
+                if self.ServiceURL.value == None:
+                    await interaction.response.send_message("Your service type is website but you did not provide a service URL. Please try again.")
+                    return
+                regex = re.search(r"(http|https)://[a-zA-Z0-9./]+", self.ServiceURL.value)
+                if regex == None:
+                    await interaction.response.send_message("Your service URL is invalid. Please try again.")
+                    return
                 
-            #     json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
-            #     Database.add_website(interaction.user.id, self.ServiceURL.value,json)
+                json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
+                # Database.add_website(interaction.user.id, self.ServiceURL.value,json)
 
                 
 
-            #     await interaction.response.send_message("Subscribed to website " + self.ServiceURL.value)
+                await interaction.response.send_message("Subscribed to website " + self.ServiceURL.value)
 
-            # elif self.ServiceType.value.lower() == "mc":
-            #     if self.ServiceURL.value == None:
-            #         await interaction.response.send_message("Your service type is minecraft but you did not provide a service URL. Please try again.")
-            #         return
-            #     regex = re.search(r"(http|https)://[a-zA-Z0-9./]+", self.ServiceURL.value)
-            #     if regex == None:
-            #         await interaction.response.send_message("Your service URL is invalid. Please try again.")
-            #         return
+            elif self.ServiceType.value.lower() == "mc":
+                if self.ServiceURL.value == None:
+                    await interaction.response.send_message("Your service type is minecraft but you did not provide a service URL. Please try again.")
+                    return
+                regex = re.search(r"(http|https)://[a-zA-Z0-9./]+", self.ServiceURL.value)
+                if regex == None:
+                    await interaction.response.send_message("Your service URL is invalid. Please try again.")
+                    return
 
-            #     json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
-            #     Database.add_minecraft(interaction.user.id, self.ServiceURL.value,json)
+                json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
+                # Database.add_minecraft(interaction.user.id, self.ServiceURL.value,json)
 
 
-            #     await interaction.response.send_message("Subscribed to minecraft server " + self.ServiceURL.value)
+                await interaction.response.send_message("Subscribed to minecraft server " + self.ServiceURL.value)
             
-            # elif self.ServiceType.value.lower() == "bot":
-            #     if self.Bot.value == None:
-            #         await interaction.response.send_message("Your service type is bot but you did not provide a bot. Please try again.")
-            #         return
-            #     regex = re.search(r"[a-zA-Z0-9]+#[0-9]{4}", self.Bot.value)
-            #     if regex == None:
-            #         await interaction.response.send_message("Your bot is not. Please try again.")
-            #         return
+            elif self.ServiceType.value.lower() == "bot":
+                if self.Bot.value == None:
+                    await interaction.response.send_message("Your service type is bot but you did not provide a bot. Please try again.")
+                    return
+                regex = re.search(r"[a-zA-Z0-9]+#[0-9]{4}", self.Bot.value)
+                if regex == None:
+                    await interaction.response.send_message("Your bot is not. Please try again.")
+                    return
 
-            #     json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
-            #     Database.add_bot(interaction.user.id, self.Bot.value,json)
+                json = await generate_json(self.NotificationType.value, self.NotificationTarget.value, self.NotificationPaylod.value)
+                # Database.add_bot(interaction.user.id, self.Bot.value,json)
 
-            #     await interaction.response.send_message("Subscribed to bot " + self.Bot.value)
+                await interaction.response.send_message("Subscribed to bot " + self.Bot.value)

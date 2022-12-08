db8ea4478f6b3da1f92f9c23e291af98b73fbfe8
Restyled.io <commits@restyled.io>
2022-12-08 11:46:03 +0000

    Restyled by black

--- app/app.py
+++ app/app.py
@@ -49,10 +49,10 @@ bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 # quart specific  configuration
 
 app = Quart(__name__)
-app.secret_key = os.getenv('SECRET_KEY')
-app.config['DISCORD_CLIENT_ID'] = os.getenv('DISCORD_CLIENT_ID')
-app.config['DISCORD_CLIENT_SECRET'] = os.getenv('DISCORD_CLIENT_SECRET')
-app.config['DISCORD_REDIRECT_URI'] = "os.getenv('DISCORD_REDIRECT_URI')"
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["DISCORD_CLIENT_ID"] = os.getenv("DISCORD_CLIENT_ID")
+app.config["DISCORD_CLIENT_SECRET"] = os.getenv("DISCORD_CLIENT_SECRET")
+app.config["DISCORD_REDIRECT_URI"] = "os.getenv('DISCORD_REDIRECT_URI')"
 oauth = DiscordOAuth2Session(app)
 
 # app.register_blueprint(api)
@@ -62,10 +62,16 @@ app.register_blueprint(routing)
 
 # bot stuff
 
+
 @bot.event
 async def on_ready():
     # on ready we will start the web server, set the bot's status and log the uptime as well as a few other admin things
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="over your bots"))
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(
+            type=discord.ActivityType.watching, name="over your bots"
+        ),
+    )
 
     # load the cogs
     for cog in cogs:
@@ -77,7 +83,10 @@ async def on_ready():
 
     log_channel = bot.get_channel(949388038260273193)
     embed = discord.Embed(
-        title="Bot Started", description=f"Succesful bot startup at {datetime.datetime.now().strftime('%d/%m/%Y at %H:%M')}", color=0x00ff00)
+        title="Bot Started",
+        description=f"Succesful bot startup at {datetime.datetime.now().strftime('%d/%m/%Y at %H:%M')}",
+        color=0x00FF00,
+    )
     await log_channel.send(embed=embed)
 
     # start checking the services
@@ -91,11 +100,12 @@ async def on_ready():
     await bot.tree.sync(guild=discord.Object(939479619587952640))
 
     print(
-        f"Sucesfully logged in as {bot.user.name}#{bot.user.discriminator} with loaded cogs {cogs}")
+        f"Sucesfully logged in as {bot.user.name}#{bot.user.discriminator} with loaded cogs {cogs}"
+    )
 
 
 async def check_applications():
-    """ Checks the applications every 5 minutes """
+    """Checks the applications every 5 minutes"""
 
     while True:
         await asyncio.sleep(300)
@@ -119,8 +129,12 @@ async def on_command_error(ctx, error):
 
     log_channel = bot.get_channel(949388038260273193)
     embed = discord.Embed(
-        title="Error", description=f"An error occured: {error} \n In Guild: {ctx.guild.name} \n Guild Owner: {ctx.guild.owner.mention}", color=0xff0000)
+        title="Error",
+        description=f"An error occured: {error} \n In Guild: {ctx.guild.name} \n Guild Owner: {ctx.guild.owner.mention}",
+        color=0xFF0000,
+    )
     await log_channel.send(embed=embed)
+
 
 if __name__ == "__main__":
     bot.run(os.environ["DISCORD_TOKEN"])
--- app/views/buttons.py
+++ app/views/buttons.py
@@ -5,5 +5,10 @@ class Feedback(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-        self.add_item(discord.ui.Button(label='Feedback Form',
-                      url="https://forms.gle/aGccPTY8u8QNemzt7", style=discord.ButtonStyle.link))
+        self.add_item(
+            discord.ui.Button(
+                label="Feedback Form",
+                url="https://forms.gle/aGccPTY8u8QNemzt7",
+                style=discord.ButtonStyle.link,
+            )
+        )
